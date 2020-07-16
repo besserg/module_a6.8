ac0550469e5aeca42d6599bc770be1f3bbe6590e
besserg <52867218+besserg@users.noreply.github.com>
2020-07-16 23:33:05 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,22 +9,37 @@ $('.j-btn1').click(function(){
 	let instr=$('.j-pr-bar').attr("style");
 	let prBarPerc=addperc(1,instr);
 	console.log(prBarPerc); 
-	$('.j-pr-bar').attr("style","width:"+prBarPerc+"%");
-	document.getElementById("pBar").textContent=prBarPerc+'%';
+	if (prBarPerc <= 100) {
+		$('.j-pr-bar').attr("style","width:"+prBarPerc+"%");
+		document.getElementById("pBar").textContent=prBarPerc+'%';
+	}
+	else {
+		alert('Не может быть больше 100%')
+	}
 });
 
 $('.j-btn3').click(function(){
 	let instr=$('.j-pr-bar').attr("style");
 	let prBarPerc=addperc(3,instr);
 	console.log(prBarPerc); 
-	$('.j-pr-bar').attr("style","width:"+prBarPerc+"%");
-	document.getElementById("pBar").textContent=prBarPerc+'%';
+	if (prBarPerc <= 100) {
+		$('.j-pr-bar').attr("style","width:"+prBarPerc+"%");
+		document.getElementById("pBar").textContent=prBarPerc+'%';
+	}
+	else {
+		alert('Не может быть больше 100%')
+	}
 });
 
 $('.j-btn7').click(function(){
 	let instr=$('.j-pr-bar').attr("style");
 	let prBarPerc=addperc(7,instr);
-	console.log(prBarPerc); 
-	$('.j-pr-bar').attr("style","width:"+prBarPerc+"%");
-	document.getElementById("pBar").textContent=prBarPerc+'%';
+	console.log(prBarPerc);
+	if (prBarPerc <= 100) { 
+		$('.j-pr-bar').attr("style","width:"+prBarPerc+"%");
+		document.getElementById("pBar").textContent=prBarPerc+'%';
+	}
+	else {
+		alert('Не может быть больше 100%')
+	}
 });
